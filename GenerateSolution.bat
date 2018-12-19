6c4cc88e8ed3328f7316be99b977ef6eecfc2f08
jkapostins@gnarlysoft.net <JKapostins@gnarlysoft.net>
2018-12-19 17:30:20 -0500

    Adding aws sdk submodule.

--- GenerateSolution.bat
+++ GenerateSolution.bat
@@ -1,9 +1,16 @@
 @echo off
 set "buildRoot=%~dp0\build\"
 
+set "awsRoot=%~dp0\third-party\aws-sdk-cpp"
+if not exist %awsRoot% (
+call git submodule init
+call git submodule update
+)
+
 if not exist %buildRoot% (
 mkdir %buildRoot%
 )
+
 cd %buildRoot%
 call cmake -G "Visual Studio 15" ../
 
