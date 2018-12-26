4de5d7093b59024bf8fea7ded41189dbf0beddb0
jkapostins@gnarlysoft.net <JKapostins@gnarlysoft.net>
2018-12-26 16:59:14 -0500

    Removing aws sdk submodule since we will be using grpc to communicate between a c# backend and the in game overlay.

--- GenerateSolution.bat
+++ GenerateSolution.bat
@@ -1,17 +1,11 @@
 @echo off
-set "buildRoot=%~dp0\build\"
-
-set "awsRoot=%~dp0\third-party\aws-sdk-cpp"
-if not exist %awsRoot% (
-call git submodule init
-call git submodule update
-)
+set "buildRoot=%~dp0\local\windows-x86"
 
 if not exist %buildRoot% (
 mkdir %buildRoot%
 )
 
 cd %buildRoot%
-call cmake -G "Visual Studio 15" ../
+call cmake -G "Visual Studio 15" ../../
 
 pause
