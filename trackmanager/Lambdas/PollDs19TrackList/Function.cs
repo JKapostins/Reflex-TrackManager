8bfc3f6e85ad7c2970f275ab7738364d3a2ced88
jkapostins@gnarlysoft.net <JKapostins@gnarlysoft.net>
2019-01-23 18:15:01 -0500

    Adding stub for lambda to parse ds19's site.

---
+++ trackmanager/Lambdas/PollDs19TrackList/Function.cs
@@ -0,0 +1,23 @@
+
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace PollDs19TrackList
+{
+    public class Function
+    {
+        
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public void FunctionHandler(ILambdaContext context)
+        {
+            //GNARLY_TODO: parse ds19's track list for new tracks.
+        }
+    }
+}
