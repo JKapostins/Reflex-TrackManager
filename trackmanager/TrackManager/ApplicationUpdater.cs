3ebfcacd2b2cb3ed4dd89cffef4fd444415b0fe5
jkapostins@gnarlysoft.net <JKapostins@gnarlysoft.net>
2019-01-27 16:52:16 -0500

    Bumping version number.

--- trackmanager/TrackManager/ApplicationUpdater.cs
+++ trackmanager/TrackManager/ApplicationUpdater.cs
@@ -51,6 +51,6 @@ namespace TrackManager
         }
 
         private string m_activeVersion;
-        private const string Version = "0.6";
+        private const string Version = "0.7.101";
     }
 }
